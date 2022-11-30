4d3202716886eaf033e35a20ec3a025dae651b3a
Michael <michael@MacBook-Pro-tilhrende-Michael.local>
2022-11-30 12:28:16 +0100

    set up react project

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import CompanyStructurePage from "./pages/CompanyStructurePage";
+
+const App = () => {
+  return (
+    <div className="App">
+      <CompanyStructurePage />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/pages/CompanyStructurePage.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const CompanyStructurePage: FC = () => {
+  return <div>Company structure page</div>;
+};
+
+export default CompanyStructurePage;
